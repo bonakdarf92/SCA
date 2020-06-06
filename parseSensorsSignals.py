bcb020aa77accf0f8bd11b2671300f4f7168bb62
bonakdarf92 <bonakdarf92@hotmail.de>
2020-06-06 19:35:45 +0200

    comaprison_recons

--- parseSensorsSignals.py
+++ parseSensorsSignals.py
@@ -5,15 +5,15 @@ import numpy as np
 with open('.\\Darmstadt_verkehr\\sensor_config.json') as f:
     sensors = json.load(f)
 
-flag = "Small"
+flag = "Big"
 knoten = ["A003", "A004", "A005", "A006", "A007", "A023", "A022", "A028", "A029", "A045", "A030", "A139", "A102", "A104"]
 knoten_darmstadt = ["A160","A161","A075","A174","A046","A012","A059","A110","A111","A173","A037","A162","A163","A104","A023","A028",
                     "A029","A030","A031","A032","A033","A001","A002","A007","A003","A004","A005","A006","A034","A036","A035","A141",
-                    "A131","A168","A043","A099","A098","A022","A045","A102","A081","A077","A084","A051","A159",
-                    "A049","A042","A182","A013","A026","A071","A027","A085","A086","A041","A017","A181","A019","A016",
-                    "A014","A021","A100","A066","A057","A048","A020","A103","A015","A151","A083",
+                    "A131","A168","A043","A099","A098","A022","A045","A102","A081","A077","A084","A051","A159","A009",
+                    "A049","A042","A013","A026","A071","A027","A085","A086","A041","A017","A019","A016",
+                    "A014","A021","A100","A066","A057","A048","A020","A103","A015","A151","A083","A134",
                     "A067","A155","A154","A128","A137","A170","A169","A147","A142","A144","A146","A136","A095",
-                    "A096","A097","A010","A089","A008","A080","A090","A069","A070","A126","A076","A011",
+                    "A096","A097","A010","A008","A080","A090","A069","A070","A126","A076","A011",
                     "A150","A061","A150","A024"]
 new_sensors = {item["ID"]:item for item in sensors}
 
@@ -62,6 +62,48 @@ if flag == "Big":
         elif j == "A174":
             new_sensors[j]["signals"] = np.empty((1440,15))
             new_sensors[j]["signals"][:] = np.nan
+        elif j == "A009":
+            new_sensors[j]["signals"] = np.empty((1440,17))
+            new_sensors[j]["signals"][:] = np.nan
+        elif j == "A076":
+            new_sensors[j]["signals"] = np.empty((1440,31))
+            new_sensors[j]["signals"][:] = np.nan
+        elif j == "A134":
+            new_sensors[j]["signals"] = np.empty((1440,11))
+            new_sensors[j]["signals"][:] = np.nan
+        elif j == "A126":
+            new_sensors[j]["signals"] = np.empty((1440,26))
+            new_sensors[j]["signals"][:] = np.nan
+        elif j == "A070":
+            new_sensors[j]["signals"] = np.empty((1440,16))
+            new_sensors[j]["signals"][:] = np.nan
+        elif j == "A080":
+            new_sensors[j]["signals"] = np.empty((1440,9))
+            new_sensors[j]["signals"][:] = np.nan
+        elif j == "A066":
+            new_sensors[j]["signals"] = np.empty((1440,16))
+            new_sensors[j]["signals"][:] = np.nan
+        elif j == "A168":
+            new_sensors[j]["signals"] = np.empty((1440,23))
+            new_sensors[j]["signals"][:] = np.nan
+        elif j == "A013":
+            new_sensors[j]["signals"] = np.empty((1440,32))
+            new_sensors[j]["signals"][:] = np.nan
+        elif j == "A041":
+            new_sensors[j]["signals"] = np.empty((1440,31))
+            new_sensors[j]["signals"][:] = np.nan
+        elif j == "A128":
+            new_sensors[j]["signals"] = np.empty((1440,16))
+            new_sensors[j]["signals"][:] = np.nan
+        elif j == "A142":
+            new_sensors[j]["signals"] = np.empty((1440,29))
+            new_sensors[j]["signals"][:] = np.nan
+        elif j == "A136":
+            new_sensors[j]["signals"] = np.empty((1440,12))
+            new_sensors[j]["signals"][:] = np.nan
+        elif j == "A096":
+            new_sensors[j]["signals"] = np.empty((1440,10))
+            new_sensors[j]["signals"][:] = np.nan
         elif j == "A046":
             new_sensors[j]["signals"] = np.empty((1440,28))
             new_sensors[j]["signals"][:] = np.nan
@@ -72,10 +114,19 @@ if flag == "Big":
             new_sensors[j]["signals"] = np.empty((1440,24))
             new_sensors[j]["signals"][:] = np.nan
         elif j == "A110":                                                   # abfangen
-            new_sensors[j]["signals"] = np.empty((1440,13))
+            new_sensors[j]["signals"] = np.empty((1440,14))
             new_sensors[j]["signals"][:] = np.nan
         elif j == "A111":
             new_sensors[j]["signals"] = np.empty((1440,7))
+            new_sensors[j]["signals"][:] = np.nan
+        elif j == "A081":
+            new_sensors[j]["signals"] = np.empty((1440,28))
+            new_sensors[j]["signals"][:] = np.nan
+        elif j == "A035":
+            new_sensors[j]["signals"] = np.empty((1440,14))
+            new_sensors[j]["signals"][:] = np.nan
+        elif j == "A036":
+            new_sensors[j]["signals"] = np.empty((1440,31))
             new_sensors[j]["signals"][:] = np.nan
         elif j == "A173":
             new_sensors[j]["signals"] = np.empty((1440,11))
@@ -200,6 +251,24 @@ if flag == "Big":
         elif j == "A170":
             new_sensors[j]["signals"] = np.empty((1440,24))
             new_sensors[j]["signals"][:] = np.nan
+        elif j == "A084":
+            new_sensors[j]["signals"] = np.empty((1440,4))
+            new_sensors[j]["signals"][:] = np.nan
+        elif j == "A051":
+            new_sensors[j]["signals"] = np.empty((1440,32))
+            new_sensors[j]["signals"][:] = np.nan
+        elif j == "A159":
+            new_sensors[j]["signals"] = np.empty((1440,20))
+            new_sensors[j]["signals"][:] = np.nan
+        elif j == "A049":
+            new_sensors[j]["signals"] = np.empty((1440,37))
+            new_sensors[j]["signals"][:] = np.nan
+        elif j == "A042":
+            new_sensors[j]["signals"] = np.empty((1440,15))
+            new_sensors[j]["signals"][:] = np.nan
+        elif j == "A010":
+            new_sensors[j]["signals"] = np.empty((1440,14))
+            new_sensors[j]["signals"][:] = np.nan
         elif j == "A169":
             new_sensors[j]["signals"] = np.empty((1440,6))
             new_sensors[j]["signals"][:] = np.nan
@@ -237,15 +306,15 @@ if flag == "Big":
             new_sensors[j]["signals"] = np.empty((1440,len(new_sensors[j]["detectors"])))
             new_sensors[j]["signals"][:] = np.nan 
 
-date = {'year':2020,'month':1,'day':2}
+date = {'year':2020,'month':1,'day':16}
 
 for k in trange(1440):
     data_csv = '.\\Darmstadt_verkehr\\{}_{}_{}_darmstadtUI\\{}.csv'.format(date["year"],date["month"],date["day"],k)
     try:
         my_data = np.genfromtxt(data_csv,delimiter=';',dtype=object,skip_header=0,skip_footer=2,deletechars="\r")
-        for j in knoten:
-            if ((j != "A030") and (j != "A102") and (j != "A104") and (j != "A160") and (j != "A100") and (j!="A20") 
-                and (j != "A103") and (j != "A170") and (j != "A010") and (j != "A080") and (j != "A090") and (j!="A070") and (j != "A150")):
+        for j in knoten_darmstadt:
+            if ((j != "A030") and (j != "A102") and (j != "A104") and (j != "A160") and (j != "A100") and (j!="A020") 
+                and (j != "A103") and (j != "A170") and (j != "A010") and (j != "A080") and (j != "A090") and (j!="A070") and (j != "A150") and (j != "A110")):
                 selec = my_data[:,2] == bytes(j.replace('0',' '),'utf-8')
             elif j == "A030":
                 selec = my_data[:,2] == b"A 30" #bytes(j.replace('0',' '),'utf-8')
@@ -272,13 +341,16 @@ for k in trange(1440):
             elif j == "A103":
                 selec = my_data[:,2] == b"A103"
             elif j == "A102":
-                selec = my_data[:,2] == b"A102" #bytes(j.replace('0',' '),'utf-8')
+                selec = my_data[:,2] == b"A102"
             elif j == "A104":
-                selec = my_data[:,2] == b"A104" #bytes(j.replace('0',' '),'utf-8')
+                selec = my_data[:,2] == b"A104"
+            #else:
+            #    selec = 
             new_sensors[j]["signals"][k,:] = np.array([k.decode().split('\\r')[0] for k in my_data[selec,6].astype('S')])
 
     except (FileNotFoundError,ValueError,OSError) as e:
         print("CSV {} nicht vorhanden oder Signal {} fehlt".format(data_csv, j))
+        print(e)
         pass
 
 
